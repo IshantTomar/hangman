f1c688e9a3c6129bcd723d3606df12a146cd362d
Ishant <ishanttomar26@gmail.com>
2024-07-19 11:36:50 +0530

    Sticky discovered

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 from customtkinter import *
 from words import words
+
 app = CTk()
 app.title("Hangman")
 
@@ -8,6 +9,7 @@ word = random.choice(words).upper()
 
 wordvar = StringVar()
 guess_en_var = StringVar()
+
 
 def check_input():
     user_input = guess_en_var.get()
@@ -19,6 +21,7 @@ def check_input():
         guess_en.delete(0)
     app.after(500, check_input)
 
+
 wordlabel = CTkLabel(app, textvariable=wordvar)
 wordlabel.grid(row=0, column=0)
 guesslabel = CTkLabel(app, text="Guess: ")
@@ -26,7 +29,7 @@ guesslabel.grid(row=1, column=0)
 guess_en = CTkEntry(app, textvariable=guess_en_var, width=30)
 guess_en.grid(row=1, column=1, sticky="w")
 guess_en_check = CTkLabel(app, text="", justify=LEFT)
-guess_en_check.grid(row=2,column=1)
+guess_en_check.grid(row=2, column=1)
 
 check_input()
 app.mainloop()
